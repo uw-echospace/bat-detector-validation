d834c815ec83f1716fbb95013e51362739591c75
Liuyixin Shao <102640018+FXF48-Mila@users.noreply.github.com>
2023-11-20 11:38:45 -0800

    Upload the python file that download the WAV files from the cloud

---
+++ data/download_wav.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+# path to the randomized csv file
+csv_file = 'randomization/selection_under_randomization_file.csv'
+# alias + bucket name in format 'alias:/bucket_name'
+bucket_name = 'something you need to change'
+# path to the folder that you want to save
+save_folder = 'something you need to change'
+
+with open(csv_file, 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        # find the 'Sliced File Path' column in the randomized file
+        # 'Sliced File Path' has the format: ubna_data_###/recover-###/UBNA_###/###_###.WAV
+        folder_path = row['Sliced File Path']
+
+        # build and execute the rclone command
+        rclone_command = f"rclone copy {bucket_name}/{folder_path} {save_folder}"
+        os.system(rclone_command)
